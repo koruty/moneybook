02190ca73d24a89aa6ccfdcf08e8f72a61a01239
koruty <124110592+koruty@users.noreply.github.com>
2023-06-16 00:31:35 +0900

    Add files via upload
    가계부 프로젝트 첫 커밋

---
+++ moneybook/src/main/java/com/joe/moneybook/diary/diaryDAO.java
@@ -0,0 +1,10 @@
+package com.joe.moneybook.diary;
+
+public class diaryDAO {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ moneybook/src/main/java/com/joe/moneybook/diary/diaryVO.java
@@ -0,0 +1,10 @@
+package com.joe.moneybook.diary;
+
+public class diaryVO {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
